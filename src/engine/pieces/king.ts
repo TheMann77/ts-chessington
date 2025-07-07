87d1a65dd3d237e1582c52d5b274de1f59993f4e
Anthony.Chan@softwire.com <Anthony.Chan@softwire.com>
2025-07-07 10:32:56 +0100

    Made king move, or something...

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import {moveByVector} from "../helperFunctions";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,18 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves =  new Array(0);
+        let current_location = board.findPiece(this);
+
+        moves.push(...moveByVector(-1, 0, current_location))
+        moves.push(...moveByVector(1, 0, current_location))
+        moves.push(...moveByVector(0, -1, current_location))
+        moves.push(...moveByVector(0, 1, current_location))
+        moves.push(...moveByVector(-1, -1, current_location))
+        moves.push(...moveByVector(1, 1, current_location))
+        moves.push(...moveByVector(1, -1, current_location))
+        moves.push(...moveByVector(-1, 1, current_location))
+
+        return moves;
     }
 }
