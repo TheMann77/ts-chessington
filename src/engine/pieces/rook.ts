72564fa6205a5f5b825b6991c25e3006c25ec77b
Anthony.Chan@softwire.com <Anthony.Chan@softwire.com>
2025-07-07 10:04:30 +0100

    Made rook move, or something...

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,25 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves =  new Array(0);
+        let current_location = board.findPiece(this);
+        let current_row = current_location.row;
+        let current_col = current_location.col;
+
+        for (let i = current_row - 1; i >= 0; i--) {
+            moves.push(Square.at(i, current_col));
+        }
+        for (let i = current_row + 1; i <= 7; i++) {
+            moves.push(Square.at(i, current_col));
+        }
+        for (let i = current_col - 1; i >= 0; i--) {
+            moves.push(Square.at(current_row, i));
+        }
+        for (let i = current_col + 1; i <= 7; i++) {
+            moves.push(Square.at(current_row, i));
+        }
+
+        moves.push();
+        return moves;
     }
 }
